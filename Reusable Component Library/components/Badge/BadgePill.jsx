a93f7c6f274cb8e31a91bce7f980efba4ceb36e8
Korhan Aladağ <50011233+KorhanAladag@users.noreply.github.com>
2023-10-09 14:12:46 +0300

    Delete Reusable Component Library directory

--- Reusable Component Library/components/Badge/BadgePill.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react" 
-
-export default function Badge( { children, color }){
-    return (
-        <div className={`badge pill ${color}`}>
-            {children}
-        </div>
-    )
-}
--- Reusable Component Library/components/Badge/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react" 
-import Badge from "./Badge"
-import BadgePill from "./BadgePill"
-import BadgeSquare from "./BadgeSquare"
-
-Badge.Pill = BadgePill
-Badge.Square = BadgeSquare
-
-export default Badge
